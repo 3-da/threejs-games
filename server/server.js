39b557aa7a21d314470ab11ff563e69aeffd13b8
3DA <3da@darijanavric.dev>
2024-09-19 09:56:45 +0200

    6_3 done

--- server/server.js
+++ server/server.js
@@ -3,11 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'start' folder
-app.use('/assets', express.static(path.join(__dirname, '../assets')));
-app.use('/libs', express.static(path.join(__dirname, '../libs')));
-app.use('/start', express.static(path.join(__dirname, '../start')));
-app.use('/complete', express.static(path.join(__dirname, '../complete')));
+// Serve static files from the 'threejs-games' folder
+app.use(express.static(path.join(__dirname, '../')));
 
 // Serve the index.html file
 app.get('/', (req, res) => {
